d6b1a9ba7919cc3f9fb7dbcd6e12a17f7a28f657
yunusemrealtug <yunusemrealtug64@gmail.com>
2024-05-20 18:14:52 +0300

    pushed random_forest

--- HW_2/decision_tree.py
+++ HW_2/decision_tree.py
@@ -1,5 +1,6 @@
 import numpy as np
 from sklearn.tree import DecisionTreeClassifier, plot_tree
+from sklearn.ensemble import RandomForestClassifier
 from sklearn.linear_model import LogisticRegression
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score
@@ -78,3 +79,29 @@ for num_features in num_features_to_select:
     # Calculate accuracy
     accuracy_lr = accuracy_score(y_test, y_pred_lr)
     print(f"Accuracy with {num_features} significant features: {accuracy_lr:.4f}")
+
+# Train a Random Forest Classifier
+train_results = []
+test_results = []
+
+for num_tree in range(1, 30):
+    clf_rf = RandomForestClassifier(n_estimators=num_tree, random_state=42)
+    clf_rf.fit(X_train, y_train)
+    y_random_forest = clf_rf.predict(X_test)
+    y_trian_random_forest = clf_rf.predict(X_train)
+    accuracy_rf = accuracy_score(y_test, y_random_forest)
+    accuracy_rf_train = accuracy_score(y_train, y_trian_random_forest)
+    print(f"Train accuracy with Random Forest with {num_tree} trees: {accuracy_rf_train:.4f}")
+    print(f"Accuracy with Random Forest with {num_tree} trees: {accuracy_rf:.4f}")
+    train_results.append(accuracy_rf_train)
+    test_results.append(accuracy_rf)
+    
+# Plot the results
+plt.figure(figsize=(10, 6))
+plt.plot(range(1,30), train_results, label='Train Accuracy')
+plt.plot(range(1,30), test_results, label='Test Accuracy')
+plt.xlabel('Number of Trees')
+plt.ylabel('Accuracy')
+plt.title('Random Forest Classifier Performance')
+plt.legend()
+plt.show()
